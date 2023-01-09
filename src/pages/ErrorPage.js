3be8c734fca173a52ddfd5ba24aaaf39bf6c2062
Nwadirioha David <40855998+Davidlloyd8@users.noreply.github.com>
2023-01-09 04:32:50 +0100

    "Update errorpage style"

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,17 +1,21 @@
 import { NavLink } from "react-router-dom";
-import "../css/errorpage.css"
+import "../css/errorpage.css";
 function ErrorPage() {
   return (
     <div className="error">
       <div class="mainbox">
         <div class="err">4</div>
-        <i class="far fa-question-circle fa-spin"></i>
+        <i class="fa fa-question-circle fa-spin"></i>
         <div class="err2">4</div>
         <div class="msg">
           Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
           existed in the first place?
           <p>
-            Let's go <NavLink to="/" className="homelink">home</NavLink> and try from there.
+            Let's go{" "}
+            <NavLink to="/" className="homelink">
+              home
+            </NavLink>{" "}
+            and try from there.
           </p>
         </div>
       </div>
